805bf64468ccf0bc280b5d1ff5e2a231428f79d2
Sergen <srgnaras@gmail.com>
2022-01-27 10:34:35 +0300

    Delete mssql_backup-master directory

--- mssql_backup-master/SQL-Backup-External.ps1
+++
@@ -1,6 +0,0 @@
-$data = Get-Date -format ddMMyyyy
-mkdir "D:\SQLBackups-External\Backup-$data"
-#xcopy.exe C:\SQLBackups\* D:\SQLBackups-External\Backup-$data /Y
-C:\Windows\System32\forfiles.exe /p "C:\SQLBackups\" /s /d 0  /c "cmd /c copy @path D:\SQLBackups-External\Backup-$data"
-
-C:\Windows\System32\forfiles.exe /p "D:\SQLBackups-External\" /s /d -7 /c "cmd /c del @file"
